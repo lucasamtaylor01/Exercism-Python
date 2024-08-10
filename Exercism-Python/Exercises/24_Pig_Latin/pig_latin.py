77af69210983ddd323e44f1a56c10c962cb5a88d
Lucas Amaral Taylor <lucasamtaylor@usp.br>
2024-08-09 23:51:06 -0300

    latin exercise update

--- Exercism-Python/Exercises/24_Pig_Latin/pig_latin.py
+++ Exercism-Python/Exercises/24_Pig_Latin/pig_latin.py
@@ -1,17 +1,48 @@
-def translate(text):
-    vogais = 'aeiou'
-    
-    if text.startswith("ay") or text.startswith("xr") or text.startswith("yt") or text[0].lower() in vogais:
-        return text + 'ay'
-    elif text[0].lower() not in vogais:
-        return rule_two_b(text)
-    
-        
-def rule_two_b(text):
+vogais = 'aeiou'
+
+def consoant_counter(text):
+    counter=0
+    for i in range(len(text)):
+        if text[i] not in vogais:
+            counter+=1
+        else:
+            break
+    return counter
+
+def rule_two(text, n):
     new_text = ""
-    for i in range(1, len(text)):
+    for i in range(n, len(text)):
         new_text += text[i]
     
-    return new_text + text[0] + "ay"
+    for i in range(n):
+        new_text += text[i]
+    return new_text + "ay"
 
-print(rule_two_b("pig"))
+def rule_three(text):
+    new_text = ""
+    if text.startswith(("qu")):
+        for i in range(2, len(text)):
+            new_text += text[i]
+        new_text += "quay"
+
+    else:
+        for i in range(3, len(text)):
+            new_text += text[i]
+
+        for i in range(3):
+            new_text+= text[i]
+
+        new_text+= "ay" 
+    return new_text
+
+def translate(text):
+    
+    if text.startswith(("ay", "xr", "yt")) or text[0].lower() in vogais:
+        return text + 'ay'
+    
+    if 'qu' in text:
+        return rule_three(text) 
+
+    if text[0].lower() not in vogais:
+        return rule_two(text, consoant_counter(text))
+    
