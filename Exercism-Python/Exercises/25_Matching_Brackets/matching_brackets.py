182202881bb83a35e0e18a6adfbd099e76a9a1d7
Lucas Taylor <lucasamtaylor@usp.br>
2024-08-13 22:31:31 -0300

    exercises of the day

--- Exercism-Python/Exercises/25_Matching_Brackets/matching_brackets.py
+++ Exercism-Python/Exercises/25_Matching_Brackets/matching_brackets.py
@@ -1,27 +1,65 @@
 def is_paired(input_string):
+    # Verifica se os contadores estão corretos
+    if counter_verifier(input_string):
+        # Verifica se os fechamentos são válidos
+        return is_valid_closure(input_string)
+    else:
+        return False
+
+def counter_verifier(input_string):
+    parenteses_counter = 0
     brackets_counter = 0
     braces_counter = 0
-    parentheses_counter = 0
-
     
     for i in range(len(input_string)):
-        if input_string[i] == '[':
-            brackets_counter+=3
-        if input_string[i]== ']':
-            brackets_counter-=3
-        if input_string[i] == '{':
-            braces_counter+=2
-        if input_string[i]== '}':
-            braces_counter-=2
-        if input_string[i] == '(':
-            parentheses_counter+=1
-        if input_string[i]== ')':
-            parentheses_counter-=1
-        
-        if parentheses_counter < 0 or braces_counter < 0 or brackets_counter < 0:
-           return False
+        if input_string[i] == '(' or input_string[i] == ')':
+            parenteses_counter += 1
+        elif input_string[i] == '{' or input_string[i] == '}':
+            braces_counter += 1
+        elif input_string[i] == '[' or input_string[i] == ']':
+            brackets_counter += 1
     
-    if parentheses_counter != 0 or braces_counter != 0 or brackets_counter != 0:
+    if parenteses_counter % 2 != 0 or braces_counter % 2 != 0 or brackets_counter % 2 != 0:
         return False
     else:
         return True
+
+def is_valid_closure(input_string):
+    for i in range(len(input_string)):
+        if input_string[i] == '(':
+            if not parenteses_verify(input_string, i):
+                return False
+        elif input_string[i] == '{':
+            if not braces_verify(input_string, i):
+                return False
+        elif input_string[i] == '[':
+            if not brackets_verify(input_string, i):
+                return False
+        
+    return True
+
+def parenteses_verify(input_string, i):
+    for j in range(i + 1, len(input_string)):
+        if input_string[j] == ')':
+            return True
+        if input_string[j] == '}' or input_string[j] == ']':
+            break
+    return False
+
+def brackets_verify(input_string, i):
+    for j in range(i + 1, len(input_string)):
+        if input_string[j] == ']':
+            return True
+        if input_string[j] == '}' or input_string[j] == ')':
+            break
+    return False
+
+def braces_verify(input_string, i):
+    for j in range(i + 1, len(input_string)):
+        if input_string[j] == '}':
+            return True
+        if input_string[j] == ']' or input_string[j] == ')':
+            break
+    return False
+
+# wrong
